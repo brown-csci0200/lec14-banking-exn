133702488274f6fc540159fd78f7c0c0242e9102
Kathi Fisler <kfisler@gmail.com>
2023-02-26 08:25:44 -0500

    lec14 starter code

---
+++ src/BankingService.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+public class BankingService {
+    AccountList accounts;
+    CustomerList customers;
+
+    public BankingService(AccountList al, CustomerList cl) {
+        this.accounts = al;
+        this.customers = cl;
+    }
+
+    public double getBalance(int forAcctNum) {
+        return accounts.findAccount(forAcctNum).getBalance();
+    }
+
+    public double withdraw(int forAcctNum, double amt) {
+        return accounts.findAccount(forAcctNum).withdraw(amt);
+    }
+
+    public String login(String custname, String withPwd) {
+        Customer cust = customers.findCustomer(custname);
+        if (cust.checkPwd(withPwd)) {
+            return "Welcome";
+        } else {
+            return "Try Again";
+        }
+    }
+}
---
+++ src/CustomerList.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+
+public class CustomerList {
+    private LinkedList<Customer> customers = new LinkedList<Customer>();
+
+    public Customer addCustomer(String username, String pwd) {
+        Customer newC = new Customer(username, pwd);
+        customers.add(newC);
+        return newC;
+    }
+
+    public Customer findCustomer(String custname) {
+        for (Customer cust:customers) {
+            if (cust.nameMatches(custname)) {
+                return cust;
+            }
+        }
+        return null; // was return "Oops -- don't know this customer"
+    }
+}
